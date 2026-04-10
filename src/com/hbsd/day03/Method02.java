11d84460cc90ba72cb84d507a80439a2b4981d6e
田佳怡 <2361410766@qq.com>
2026-04-10 10:59:02 +0800

    4.10，使用switch，case实现电商折扣

--- src/com/hbsd/day03/Method02.java
+++ src/com/hbsd/day03/Method02.java
@@ -1,16 +1,23 @@
 package com.hbsd.day03;
 
 public class Method02 {
-    public static void main(String[] args) {
-        int[] num1 = {2, 3, 4, 5, 6};
-        int num[] = {1, 2, 3, 4, 5};
-
-        hhh(num1);
-        hhh(num);
-    }
-    public static void hhh(int num[]){
-        for(int i : num){
-            System.out.println(i);
+    public static double count(String grade,double price){
+        double sum_price = 0.0;
+        switch (grade){
+            case "common":
+                sum_price = (double)price;
+                break;
+            case  "VIP":
+                sum_price = (double) price * 0.9;
+                break;
+            case  "SVIP":
+                sum_price = (double) price * 0.8;
+                break;
         }
+        return sum_price;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count("VIP", 1000));
     }
 }
